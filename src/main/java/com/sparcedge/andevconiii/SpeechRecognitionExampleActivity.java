4479ff8194bca55cb966029157671a24608f0ae3
Dayel Ostraco <dayel.ostraco@gmail.com>
2012-05-16 18:19:26 -0700

    Added Wolfram Alpha results to the Speech Recognition demo.

--- src/main/java/com/sparcedge/andevconiii/SpeechRecognitionExampleActivity.java
+++ src/main/java/com/sparcedge/andevconiii/SpeechRecognitionExampleActivity.java
@@ -6,27 +6,47 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import com.sparcedge.andevconIII.R;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * User: dayelostraco
  * Date: 5/16/12
  * Time: 1:47 PM
  */
-public class SpeechRecognitionExampleActivity extends Activity {
+public class SpeechRecognitionExampleActivity extends Activity implements TextToSpeech.OnInitListener {
 
     public static final int REQUEST_CODE = 2;
+    private static final String WOLFRAM_ALPHA_APP_ID = "4Y754K-7RJ4J58PYV";
+
+    private TextToSpeech mTts;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.speech_demo);
+
+        mTts = new TextToSpeech(this, this);
 
         PackageManager pm = getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
@@ -35,11 +55,11 @@ public class SpeechRecognitionExampleActivity extends Activity {
             toast.show();
 
         } else {
-            setButtonActions();
+            inflateButtons();
         }
     }
 
-    private void setButtonActions(){
+    private void inflateButtons() {
         Button speakButton = (Button) findViewById(R.id.speakButton1);
         speakButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +72,46 @@ public class SpeechRecognitionExampleActivity extends Activity {
         });
     }
 
+    private void speak(String textToSpeak) {
+        mTts.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private String getAnswer(String question) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("http://api.wolframalpha.com/v2/query?input=");
+        stringBuilder.append(question.replaceAll(" ", "+"));
+        stringBuilder.append("&appid=");
+        stringBuilder.append(WOLFRAM_ALPHA_APP_ID);
+
+        try{
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(stringBuilder.toString());
+            HttpResponse response = client.execute(request);
+
+            StatusLine status = response.getStatusLine();
+            if (status.getStatusCode() != 200) {
+                Log.d("SpeechRecognitionExampleActivity", "HTTP error, invalid server status code: " + response.getStatusLine());
+            }
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(response.getEntity().getContent());
+
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+            XPathExpression expr = xpath.compile("/queryresult/pod[@title='Result']/subpod[@title='']/plaintext/text()");
+            Object result = expr.evaluate(doc, XPathConstants.STRING);
+
+            return result.toString();
+
+        } catch (Exception e){
+            Log.d("SpeechRecognitionExampleActivity", "Could not parse Wolfram Alpha response.");
+        }
+
+        return "Could not find an answer";
+    }
+
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode, Intent data) {
@@ -59,13 +119,41 @@ public class SpeechRecognitionExampleActivity extends Activity {
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
             ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
-            if(matches.size()>0){
+            if (matches.size() > 0) {
                 String bestResult = matches.get(0);
-                Toast toast = Toast.makeText(this, bestResult, 2000);
+                String answer = getAnswer(bestResult);
+                Toast toast = Toast.makeText(this, answer, 100000);
                 toast.show();
+                speak(answer);
             }
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mTts != null) {
+            mTts.stop();
+            mTts.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = mTts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("SpeechRecognitionExampleActivity", "Language is not available.");
+            }
+
+        } else {
+            Log.e("SpeechRecognitionExampleActivity", "Could not initialize TextToSpeech.");
+        }
     }
 }
